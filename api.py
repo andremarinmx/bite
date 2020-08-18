b5dccd84c510d4917966b6973b16365f3b223e08
Óscar Miranda <oscarmiranda3615@gmail.com>
2020-08-18 11:16:22 -0600

    Modificaciones en las rutas de la API
    - Cambiado a POST en /login y /register
    - Agregadas /logout y /posts

--- api.py
+++ api.py
@@ -1,11 +1,23 @@
-from flask import Blueprint
+from flask import (
+	Blueprint,
+	jsonify,
+	session
+)
 
 api = Blueprint('api', __name__)
 
-@api.route('/login')
+@api.route('/login', methods = ['POST'])
 def login_endpoint():
 	return '/login'
 
-@api.route('/register')
+@api.route('/register', methods = ['POST'])
 def register_endpoint():
     return '/register'
+
+@api.route('/logout')
+def logout_user():
+	return '/logout'
+
+@api.route('/posts')
+def get_posts():
+	return jsonify([])
