7ffdd3fce94020a05f6a97ce37e7d0563ade2712
andre707 <andre707.am@gmail.com>
2020-08-20 10:47:23 -0600

    Rutas agregadas

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ from flask import (
 	Flask,
 	render_template,
 	send_from_directory,
-	session
+	session, 
+	request
+	
 )
 from api import api
 from login import LoginHandler
@@ -22,11 +24,69 @@ login = LoginHandler(app)
 def root():
 	return render_template('index.html')
 
+@app.route('/login')
+def login():
+	return '/login'
+	
+@app.route('/register')
+def register():
+    return '/register'
+	
+@app.route('/recover')
+def recover():
+    return '/recover'
+
 @app.route('/home')
 def home():
 	return render_template('home.html')
 
+@app.route('/category/:id')
+def category(categoty_id:int):
+    return '/category/:id'
 
+@app.route('/search')
+def search():
+    return '/search'
+
+@app.route('/orders/:id')
+def orders(orders_id:int):
+    return '/orders/:id'
+
+@app.route('/opinion')
+def opinion():
+    return '/opinion'
+
+@app.route('/profile')
+def profile():
+    return '/profile'
+
+@app.route('/add_product')
+def add_product():
+    return '/add product'
+
+@app.route('/product/:id')
+def product(product_id:int):
+    return '/product/:id'
+
+@app.route('/edit_product/:id')
+def edit_product(edit_product_id:int):
+    return '/edit_product/:id'
+
+@app.route('/settings')
+def settings():
+    return '/settings'
+
+@app.route('/edit_profile')
+def edit_profile():
+	return '/edit_profile'
+
+@app.route('/about')
+def about():
+    return '/about'
+
+@app.route('/about/:page')
+def about_page(about_page:int):
+    return '/about/:page'
 
 # Archivos estáticos.
 
