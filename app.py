28eb750c4a39105d6f944d2f8c43cc8c4f50789d
Óscar Miranda <oscarmiranda3615@gmail.com>
2020-08-27 15:57:47 -0600

    Refactorización de BaseLayout.html

--- app.py
+++ app.py
@@ -105,14 +105,6 @@ def about_page(page: str):
 
 # Archivos estáticos.
 
-@app.route('/manifest.json')
-def manifest():
-	return send_from_directory('static', 'manifest.json')
-
-@app.route('/favicon.ico')
-def favicon():
-	return send_from_directory('static/icons', 'icon-72x72.png')
-
 @app.route('/icons/<string:filename>')
 def icons(filename):
 	return send_from_directory('static/icons', filename)
